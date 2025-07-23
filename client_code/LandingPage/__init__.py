deca02b5c7844455502198581fe1eb43baceafce
mvd@canvas-antler.com <mvd@canvas-antler.com>
2025-07-23 06:55:04 +0000

    Edited AboutUs, ContactUs, DataDashboard, DataDashboard.RowTemplate1 and 5 more

--- client_code/LandingPage/__init__.py
+++ client_code/LandingPage/__init__.py
@@ -1,5 +1,7 @@
 from ._anvil_designer import LandingPageTemplate
 from anvil import *
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
 import anvil.server
 import anvil.users
 import anvil.tables as tables
@@ -13,3 +15,7 @@ class LandingPage(LandingPageTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def explore_dashboard_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('DataDashboard')
