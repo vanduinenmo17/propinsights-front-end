c27a4403a9900d1de758c22f648b94e199885c65
mvd@canvas-antler.com <mvd@canvas-antler.com>
2025-09-06 21:25:06 +0000

    Edited utils

--- client_code/utils.py
+++ client_code/utils.py
@@ -5,13 +5,7 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-# This is a module.
-# You can define variables and functions here, and use them from any form. For example, in a top-level form:
-#
-#    from .. import Module1
-#
-#    Module1.say_hello()
-#
+
 DATA_LIST_COLUMNS = ['Address','City','County','State','OwnerName','OwnerAddress','OwnerCity','OwnerState',
                      'OwnerZip','BuildingDescription','SF','Bedrooms','Bathrooms','YearBuilt','AssessedValue',
                      'LastSalesPrice','LastSalesDate','LAT','LON']
